57de7b123870e20f3222bdc0df9977455ad4cd3b
snowcoding <vimalshah77@gmail.com>
2019-03-05 16:21:15 -0800

    Added the VoteType Added method to resolve_votes Added CreateVote class Added create_vote field to Mutation class

--- links/schema.py
+++ links/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from users.schema import UserType
 
-from .models import Link
+from .models import Link, Vote
 
 
 # When creating a new Type, if we Model then we can inherit the fields by using a built-in DjangoObjectType, which is
@@ -19,14 +19,23 @@ class LinkType(DjangoObjectType):
         return "Resolve F"
 
 
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+
+
 class Query(graphene.ObjectType):
     # Each element in the list  is a LinkType
     # links is an class instance of the List which is of LinkType
     links = graphene.List(LinkType)
+    votes = graphene.List(VoteType)
 
     # Resolving - what should we return in that field?
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 
 class CreateLink(graphene.Mutation):
@@ -58,6 +67,31 @@ class CreateLink(graphene.Mutation):
         )
 
 
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('You must be logged to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+
+
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
     vim = CreateLink.Field()
